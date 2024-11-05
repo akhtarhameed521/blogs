69d758daa001504243e532cad88572e6e3f4b0c4
akhtar hameed <akhtarhameed521@gmail.com>
2024-11-05 10:59:31 +0500

    fix the loading issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,33 +33,7 @@ export default function Home() {
   const handleNextPage = () => setPage((prev) => prev + 1);
   const handlePreviousPage = () => setPage((prev) => Math.max(prev - 1, 1));
 
-  if (loading) {
-    return (
-      <div className="flex items-center justify-center w-full h-screen">
-        {/* SVG Loader */}
-        <svg
-          className="animate-spin h-10 w-10 text-indigo-600"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <circle
-            className="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            strokeWidth="4"
-          ></circle>
-          <path
-            className="opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8v8H4z"
-          ></path>
-        </svg>
-      </div>
-    );
-  }
+  
 
   if (status === "unauthenticated") {
     return (
